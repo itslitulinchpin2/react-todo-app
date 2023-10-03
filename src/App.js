9c7f7fb73c9347ac78ad48ea8197006296a97f1b
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-10-03 03:49:18 +0200

    class style initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from "react";
+import "./App.css"
+export default class App extends Component {
+  render(){
+    return(
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
+//클래스 컴포넌트에서는 render() 안에서 UI관련된 코드를 작성한다.
+//jsx에서는 css를 위한 명칭으로 class대신 className을 사용한다.
