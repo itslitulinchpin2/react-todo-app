822086856eca9b7c841e83da1137f5fd2f014da8
itslitulinchpin2 <itslitulinchpin@gmail.com>
2023-10-04 04:50:33 +0200

    map and filter API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React, {Component} from "react";
 import "./App.css"
 export default class App extends Component {
+
+  btnStyle={
+    color:"#fff",
+    border:"none",
+    padding:"5px 9px",
+    borderRadius:"50%",
+    cursor:"pointer",
+    float:"right"
+  }
+
+  getStyle = () => {
+    return{
+      padding:"10px",
+      borderBottomL :"1px #ccc dotted",
+      textDecoration: "none"
+    }
+  }
+
+  todoData = [
+    {
+      id: "1",
+      title:"공부하기",
+      completed:true
+    },
+    {
+      id: "2",
+      title:"청소하기",
+      completed:false
+    }
+  ]
+
+  handleClick=(id)=>{
+    let newTodoData = this.todoData.filter(data => data.id !== id)
+    //클릭한 아이디가 아닌 아이디들만 할 일 목록에 남는다.
+    console.log('newTodoData', newTodoData);
+  }
+
   render(){
     return(
       <div className="container">
         <div className="todoBlock">
           <div className="title">
             <h1>할 일 목록</h1>
-          </div>
+            </div>
+
+
+            {this.todoData.map(data => (
+              <div style={this.getStyle()} key={data.id}>
+                <input type="checkbox" defaultChecked={data.completed}/>
+                  {data.title}
+                <button style={this.btnStyle} onClick = {()=>this.handleClick(data.id)}>x</button>
+              </div>
+            ))
+            }
+          
         </div>
       </div>
     )
